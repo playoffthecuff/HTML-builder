759af3a59a95d675fb52c5870430ef0478e92f96
playoffthecuff <jmobile@bk.ru>
2024-01-22 00:42:46 +0300

    feat: add farewell phrase and exit on events

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,12 +1,21 @@
 const fs = require('fs');
-const { stdin, stdout } = process;
+const { stdin, stdout, exit } = process;
+const path = require('path');
+const targetPath = path.join(__dirname, 'text.txt');
+
+const writeStream = fs.createWriteStream(targetPath);
 stdout.write('Hi! Leave your message below, please.\n');
 stdin.on('data', (data) => {
-  fs.writeFile('./02-write-file/text.txt', data, (err) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    process.exit();
-  });
+  if (data.toString().trim() === 'exit') {
+    stdout.write('Good bye!');
+    exit();
+  }
+  writeStream.write(data);
+});
+process.on('SIGINT', () => {
+  stdout.write('Good bye!');
+  exit();
+});
+stdin.on('end', () => {
+  exit();
 });
