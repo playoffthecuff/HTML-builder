39b5f4006af3196a403bce3d80083ee594e3db58
playoffthecuff <jmobile@bk.ru>
2024-01-22 08:07:26 +0300

    feat: add removing excess files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,22 +1,33 @@
 const fs = require('fs');
-const dir = __dirname;
-const sourceFolder = dir + '\\files';
-const targetFolder = dir + '\\files-copy';
+const path = require('path');
+const sourceFolder = path.join(__dirname, 'files');
+const targetFolder = path.join(__dirname, 'files-copy');
 
 fs.mkdir(targetFolder, { recursive: true }, (err) => {
   if (err) throw err;
-  fs.readdir(sourceFolder, (err, files) => {
+  fs.readdir(sourceFolder, (err, sourceFiles) => {
     if (err) {
       console.log(err);
       return;
     }
-    for (let file of files) {
+    for (let file of sourceFiles) {
       fs.copyFile(
-        `${sourceFolder}\\${file}`,
-        `${targetFolder}\\${file}`,
+        path.join(sourceFolder, file),
+        path.join(targetFolder, file),
         (err) => {
           if (err) console.log(err);
         },
+        fs.readdir(targetFolder, (err, targetFiles) => {
+          if (err) {
+            console.log(err);
+            return;
+          }
+          for (let file of targetFiles) {
+            if (!sourceFiles.includes(file)) {
+              fs.unlink(path.join(targetFolder, file), () => {});
+            }
+          }
+        }),
       );
     }
   });
